ffef06acf6f2116742b13e49e19bb2f1df7c993b
Diego <diegosebastianramirez29@gmail.com>
2025-09-30 12:37:28 -0600

    Primer commit

---
+++ src/app/componentes/agregar-alumno/agregar-alumno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-alumno',
+  standalone: true,
+  imports: [],
+  templateUrl: './agregar-alumno.component.html',
+  styleUrl: './agregar-alumno.component.css'
+})
+export class AgregarAlumnoComponent {
+
+}
---
+++ src/app/componentes/listar-alumno/listar-alumno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-alumno',
+  standalone: true,
+  imports: [],
+  templateUrl: './listar-alumno.component.html',
+  styleUrl: './listar-alumno.component.css'
+})
+export class ListarAlumnoComponent {
+
+}
