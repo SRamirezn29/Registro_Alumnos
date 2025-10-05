c633ba7fe13e5af37cfb8b54dc20d25c170c1299
nelson jared alvarez corea <nelsonjared.hn@gmail.com>
2025-10-05 11:02:49 -0600

    Arregle problemas del test con jasmine y karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [RouterTestingModule,CommonModule,AppComponent],
     }).compileComponents();
   });
 
@@ -20,10 +22,15 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('frontend-crud-alumnos');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend-crud-alumnos');
-  });
+  it('should render title', async () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  await fixture.whenStable();
+
+  const compiled = fixture.nativeElement as HTMLElement;
+  console.log('HTML renderizado:\n', compiled.outerHTML); 
+
+  expect(compiled.querySelector('h1')?.textContent)
+    .toContain('Hello, frontend-crud-alumnos');
+});
 });
